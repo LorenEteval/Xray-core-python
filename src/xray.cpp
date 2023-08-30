0656fde7318055a6400a7e6485047fec430aa483
Loren Eteval <loren.eteval@proton.me>
2023-08-30 18:46:21 +0800

    Bump version to 1.8.4 in xray.cpp
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- src/xray.cpp
+++ src/xray.cpp
@@ -42,6 +42,6 @@ namespace {
             "Start Xray client with JSON string",
             py::arg("json"));
 
-        m.attr("__version__") = "1.8.3";
+        m.attr("__version__") = "1.8.4";
     }
 }
